3938261f560a644799a7646c05f15fec1e823cbf
Daniel Le'Sage <daniel@partnerhero.com>
2022-10-24 19:59:47 -0600

    test: modifying coverage collection to include all files

--- jest.config.ts
+++ jest.config.ts
@@ -20,13 +20,27 @@ export default {
     collectCoverage: true,
 
     // An array of glob patterns indicating a set of files for which coverage information should be collected
-    // collectCoverageFrom: undefined,
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.ts',
+        '!**/*index.ts',
+        '!**/*commands.ts',
+        '!**/*options.ts',
+        '!**/*types.ts',
+    ],
 
     // The directory where Jest should output its coverage files
     coverageDirectory: 'coverage',
 
     // An array of regexp pattern strings used to skip coverage collection
-    coveragePathIgnorePatterns: ['<rootDir>/src/mocks/', '/node_modules/'],
+    coveragePathIgnorePatterns: [
+        '<rootDir>/src/mocks/',
+        '/node_modules/',
+        '/typings/',
+        '/prompts/',
+        '/types/',
+        '/constants/',
+        '/dtos/',
+    ],
 
     // Indicates which provider should be used to instrument code for coverage
     coverageProvider: 'v8',
